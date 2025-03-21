400002093a3728a42b6ce405e6cafbebffc04cfd
Akimenko_Ivan <412871@niuitmo.ru>
2025-03-21 19:42:43 +0300

    + log_files, main and functions

---
+++ functions.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def f1(args):
+    return args[0] ** 2 + args[1] ** 2
+
+
+def f1_1(args):
+    return (args[0] + 2) ** 2 + args[1] ** 2
+
+
+def f1_2(args):
+    return args[0] ** 2 + (args[1] - 3) ** 2
+
+
+def f1_3(args):
+    return (args[0] - 2) ** 2 + (args[1] + 1) ** 2
+
+
+B = 1.5
+
+
+def f3(args):
+    return args[0] ** 2 + B * args[0] * args[1] + args[1] ** 2
+
+
+def multimodal_f(args):
+    return 20 + args[0] ** 2 + args[1] ** 2 - 10 * np.cos(2*np.pi * args[0]) - 10 * np.cos(2 * np.pi * args[1])
---
+++ main.py
@@ -0,0 +1,38 @@
+from gradient_minimization import gradient_descent
+from scipy.optimize import minimize
+from functions import f1, f1_1, f1_2, f1_3, f3, multimodal_f, np
+
+
+methods = ["default", "decreasing_lr", "Armijo", "Goldstein", "golden_section", "dihotomiya"]
+functions = [(f1, "f1 = x^2 + y^2"),
+             (f1_1, "f1_1 = (x + 2)^2 + y^2"),
+             (f1_2, "f1_2 = x^2 + (y - 3)^2"),
+             (f1_3, "f1_3 = (x - 2)^2 + (y + 1)^2"),
+             (f3, "f3 = x^2 + Bxy + y^2, -2 < B < 2"),
+             (multimodal_f, "multimodal_f = 20 + x^2 + y^2 - 10cos(2 * pi * x) - 10cos(2 * pi * y)"),
+             ]
+
+
+def print_result():
+    for method in methods:
+        print("=" * 50)
+        print(f"Minimization with {method} method gradient descent:")
+        print("=" * 50)
+        for function in functions:
+            print(function[1])
+            print("=" * 50)
+            if method == "decreasing_lr":
+                coords = gradient_descent(function[0], 1, 2, method=method, h=0.1, iterations=50000)
+            else:
+                coords = gradient_descent(function[0], 1, 2, method=method)
+            if method == "dihotomiya" or method == "golden_section":
+                print(f"Scipy.optimize.line_search result: x={coords[2]}, y={coords[3]}")
+            if method == "Armijo" or method == "Goldstein":
+                result = minimize(fun=function[0], x0=np.array([5, -5]), method='BFGS',
+                                  options={'maxiter': 2000, 'gtol': np.finfo(float).eps})
+                print(f"Scipy BFGS result: x={result.x[0]}, y={result.x[1]}")
+            print(f"x={coords[0]}, y={coords[1]}")
+            print("=" * 50)
+
+
+print_result()
