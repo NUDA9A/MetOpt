1e26f12a158cb438325df9b50fa35bf34c6d5838
Akimenko_Ivan <412871@niuitmo.ru>
2025-04-07 17:33:51 +0300

    + newton and bfgs methods

--- gradient_minimization.py
+++ gradient_minimization.py
@@ -2,6 +2,16 @@ from functions import np
 from scipy.optimize import line_search, minimize_scalar
 
 
+def hessp_factory(f):
+    def hessp(x, p):
+        eps = np.sqrt(np.finfo(float).eps)
+        grad1 = grad_f(f, x[0] + eps * p[0], x[1] + eps * p[1])
+        grad2 = grad_f(f, x[0], x[1])
+        return (grad1 - grad2) / eps
+
+    return hessp
+
+
 def grad_f(f, x, y):
     eps = np.finfo(float).eps
     delta_x = np.sqrt(eps) * max(1.00, abs(x))
@@ -12,11 +22,30 @@ def grad_f(f, x, y):
     return np.array([df_dx, df_dy])
 
 
-def goldstein(f, x, y, grad, c1, c2, a0, iterations, log_file, counters):
-    p = -grad
+def hess_f(f, x, y):
+    eps = np.finfo(float).eps
+    delta_x = np.sqrt(eps) * max(1.00, abs(x))
+    delta_y = np.sqrt(eps) * max(1.00, abs(y))
+
+    grad_x_plus = grad_f(f, x + delta_x, y)
+    grad_x_minus = grad_f(f, x - delta_x, y)
+    grad_y_plus = grad_f(f, x, y + delta_y)
+    grad_y_minus = grad_f(f, x, y - delta_y)
+
+    dgrad_dx = (grad_x_plus - grad_x_minus) / (2 * delta_x)
+    dgrad_dy = (grad_y_plus - grad_y_minus) / (2 * delta_y)
+
+    H = np.array([
+        [dgrad_dx[0], dgrad_dy[0]],
+        [dgrad_dx[1], dgrad_dy[1]],
+    ])
+
+    return H
+
+
+def goldstein(f, x, y, p, direction, c1, c2, a0, iterations, log_file, counters):
     a_l = 0.0
     a_r = a0
-    direction = np.dot(grad, p)
 
     fxy = f([x, y])
     counters[0] += 1
@@ -42,10 +71,8 @@ def goldstein(f, x, y, grad, c1, c2, a0, iterations, log_file, counters):
     return x_k, y_k
 
 
-def armijo_gradient_descent(f, x, y, grad, c1, a0, q, log_file, counters):
-    p = -grad
+def armijo_gradient_descent(f, x, y, p, direction, c1, a0, q, log_file, counters):
     a = a0
-    direction = np.dot(grad, p)
     fxy = f([x, y])
     counters[0] += 1
 
@@ -102,7 +129,10 @@ def parabolic(f, x, y, grad, alpha_vals):
     phis = np.array([phi(alpha) for alpha in alphas])
 
     A = np.vstack([alphas ** 2, alphas, np.ones_like(alphas)]).T
-    a, b, c = np.linalg.solve(A, phis)
+    try:
+        a, b, c = np.linalg.solve(A, phis)
+    except np.linalg.LinAlgError:
+        return alphas[np.argmin(phis)]
 
     if a <= 0:
         res = alphas[np.argmin(phis)]
@@ -175,8 +205,85 @@ def s_minimize(f, x, y, grad, method):
     return x - alpha.x * grad[0], y - alpha.x * grad[1]
 
 
+def get_p_for_newton(f, x, y, grad, counters):
+    H = hess_f(f, x, y)
+    counters[0] += 16
+    counters[1] += 4
+    try:
+        p = -np.linalg.solve(H, grad)
+    except np.linalg.LinAlgError:
+        print("Гессиан вырожден")
+        p = -grad
+
+    return p
+
+
+def bfgs(
+    f,
+    x, y,
+    B,
+    prev_args,
+    prev_grad,
+    grad,
+    iteration,
+    log_file,
+    counters,
+    c1, a0, q
+):
+    if iteration == 0:
+        B = np.eye(2)
+        prev_args = np.array([x, y])
+        prev_grad = grad
+        p = -grad / (np.linalg.norm(grad) + 1e-12)
+    else:
+        x_prev = prev_args
+        s = np.array([x, y]) - x_prev
+        y_vec = grad - prev_grad
+        Bs = B.dot(s)
+
+        ys = y_vec @ s
+        sBs = s @ Bs
+
+        if ys > 1e-12 and sBs > 1e-12:
+            B += np.outer(y_vec, y_vec)/ys - np.outer(Bs, Bs)/sBs
+
+        prev_args = np.array([x, y])
+        prev_grad = grad
+
+        p = -B.dot(grad)
+
+    direction = np.dot(grad, p)
+    x_k, y_k = armijo_gradient_descent(
+        f=f,
+        x=x,
+        y=y,
+        p=p,
+        direction=direction,
+        c1=c1,
+        a0=a0,
+        q=q,
+        log_file=log_file,
+        counters=counters
+    )
+
+    s2 = np.array([x_k - x, y_k - y])
+    grad_new = grad_f(f, x_k, y_k)
+    y_vec2 = grad_new - grad
+    Bs2 = B.dot(s2)
+
+    ys2 = y_vec2 @ s2
+    sBs2 = s2 @ Bs2
+
+    if ys2 > 1e-12 and sBs2 > 1e-12:
+        B += np.outer(y_vec2, y_vec2)/ys2 - np.outer(Bs2, Bs2)/sBs2
+
+    log_file.write(f"{x_k} {y_k}\n")
+
+    return x_k, y_k, -1, -1, B, np.array([x_k, y_k]), grad_new
+
+
 def make_step(
-        f, x, y, h,
+        f, x, y, h, newton_h,
         l_s_x,
         l_s_y,
         grad,
@@ -184,54 +291,79 @@ def make_step(
         method,
         iteration,
         log_file,
-        c1, c2,
+        c1, c2, q,
         a_0,
         stop,
-        counters
+        counters,
+        B, prev_args, prev_grad
 ):
     match method:
         case "default":
-            return x - h * grad[0], y - h * grad[1], -1, -1
+            return x - h * grad[0], y - h * grad[1], -1, -1, None, None, None
         case "decreasing_lr":
-            return x - (h / np.sqrt((iteration + 1))) * grad[0], y - (h / np.sqrt((iteration + 1))) * grad[1], -1, -1
+            return x - (h / np.sqrt((iteration + 1))) * grad[0], y - (h / np.sqrt((iteration + 1))) * grad[1], -1, -1, None, None, None
         case "Armijo":
-            a_x, a_y = armijo_gradient_descent(f, x, y, grad, c1, a_0, 0.5, log_file, counters)
+            a_x, a_y = armijo_gradient_descent(f, x, y, -grad, np.dot(grad, -grad), c1, a_0, q, log_file, counters)
             ls_x, ls_y = l_search(f, l_s_x, l_s_y, grad_l_s, a_0, c1, 0.9)
-            return a_x, a_y, ls_x, ls_y
+            return a_x, a_y, ls_x, ls_y, None, None, None
         case "Goldstein":
-            g_x, g_y = goldstein(f, x, y, grad, c1, c2, a_0, 100, log_file, counters)
+            g_x, g_y = goldstein(f, x, y, -grad, np.dot(grad, -grad), c1, c2, a_0, 100, log_file, counters)
             ls_x, ls_y = l_search(f, l_s_x, l_s_y, grad_l_s, a_0, c1, c2)
-            return g_x, g_y, ls_x, ls_y
+            return g_x, g_y, ls_x, ls_y, None, None, None
         case "golden_section":
             _, a2, _ = golden_section(f, x, y, grad, 0.0, a_0, counters, stop)
             sm_x, sm_y = s_minimize(f, l_s_x, l_s_y, grad_l_s, "golden")
 
-            return x - a2 * grad[0], y - a2 * grad[1], sm_x, sm_y
+            return x - a2 * grad[0], y - a2 * grad[1], sm_x, sm_y, None, None, None
         case "dihotomiya":
             alpha = dihotomiya(f, x, y, grad, 0.0, a_0, counters, stop)
-            return x - alpha * grad[0], y - alpha * grad[1], -1, -1
+            return x - alpha * grad[0], y - alpha * grad[1], -1, -1, None, None, None
         case "parabolic":
             a1, a2, a3 = golden_section(f, x, y, grad, 0.0, a_0, counters, stop)
             alpha = parabolic(f, x, y, grad, [a1, a2, a3])
             counters[0] += 3
             counters[2] += 1
             s_x, s_y = s_minimize(f, l_s_x, l_s_y, grad_l_s, "brent")
-            return x - alpha * grad[0], y - alpha * grad[1], s_x, s_y
+            return x - alpha * grad[0], y - alpha * grad[1], s_x, s_y, None, None, None
+        case "newton":
+            p = get_p_for_newton(f, x, y, grad, counters)
+
+            return x + newton_h * p[0], y + newton_h * p[1], -1, -1, None, None, None
+        case "newton_armijo":
+            p = get_p_for_newton(f, x, y, grad, counters)
+
+            direction = np.dot(grad, p)
+
+            a_x, a_y = armijo_gradient_descent(f, x, y, p, direction, c1, a_0, q, log_file, counters)
+
+            return a_x, a_y, -1, -1, None, None, None
+        case "bfgs":
+            return bfgs(f, x, y, B, prev_args, prev_grad, grad, iteration, log_file, counters, c1, a_0, q)
 
 
 def gradient_descent(
         f, x0, y0,
         method="default",
         h=0.01,
+        newton_h=1,
         iterations=2000,
         stop=np.finfo(float).eps,
-        c1=0.3, c2=0.7,
+        c1=0.3, c2=0.7, q=0.5,
         a_0=2.0
 ):
     x, y = x0, y0
     l_s_x, l_s_y = x0, y0
     counters = [0, 0, 0]
     with open(f.__name__ + "_" + method + ".txt", "w") as log_file:
+        if method == "bfgs":
+            B = np.eye(2)
+            prev_args = np.array([x, y])
+            prev_grad = grad_f(f, x, y)
+            counters[1] += 1
+        else:
+            B = None
+            prev_args = None
+            prev_grad = None
         for i in range(iterations):
             counters[2] += 1
             log_file.write(f"{x} {y}" + "\n")
@@ -241,12 +373,17 @@ def gradient_descent(
             counters[0] += 4
             if np.linalg.norm(grad) < stop:
                 break
-            x, y, l_s_x, l_s_y = make_step(
-                f, x, y, h,
+            prev_x, prev_y = x, y
+            x, y, l_s_x, l_s_y, B, prev_args, prev_grad = make_step(
+                f, x, y, h, newton_h,
                 l_s_x, l_s_y,
                 grad,
                 grad_l_s,
-                method, i, log_file, c1, c2, a_0, stop, counters,
+                method, i, log_file, c1, c2, q, a_0, stop, counters,
+                B, prev_args, prev_grad
             )
+            delta = np.linalg.norm([x - prev_x, y - prev_y])
+            if (method == "newton" or method == "newton_armijo") and np.linalg.norm(delta) < stop:
+                break
 
     return [x, y, l_s_x, l_s_y, counters]
--- main.py
+++ main.py
@@ -1,15 +1,60 @@
-from gradient_minimization import gradient_descent
-from functions import f1, f1_1, f1_2, f1_3, f3, multimodal_f, noisy_multimodal_f
+import numpy as np
 
-methods = ["default", "decreasing_lr", "Armijo", "Goldstein", "golden_section", "dihotomiya", "parabolic"]
+from gradient_minimization import gradient_descent, grad_f, hessp_factory
+from functions import f1, f1_1, f1_2, f1_3, f3, multimodal_f, noisy_multimodal_f
+from scipy.optimize import minimize
+
+methods = [
+    # "default",
+    # "decreasing_lr",
+    # "Armijo",
+    # "Goldstein",
+    # "golden_section",
+    # "dihotomiya",
+    # "parabolic",
+    # "newton",
+    # "newton_armijo",
+    # "Nelder-Mid",
+    "bfgs",
+]
 functions = [(f1, "f1 = x^2 + y^2"),
              (f1_1, "f1_1 = (x + 2)^2 + y^2"),
              (f1_2, "f1_2 = x^2 + (y - 3)^2"),
              (f1_3, "f1_3 = (x - 2)^2 + (y + 1)^2"),
              (f3, "f3 = x^2 + Bxy + y^2, -2 < B < 2"),
              (multimodal_f, "multimodal_f = 20 + x^2 + y^2 - 10cos(2 * pi * x) - 10cos(2 * pi * y)"),
-             (noisy_multimodal_f, "noisy_multimodal_f = ∑ sin(m·x)·cos(m·y), m = 1..M + N(0, σ)")
+             (noisy_multimodal_f, "noisy_multimodal_f = ∑ sin(m·x)·cos(m·y), m = 1..M + N(0, σ)"),
              ]
+
+
+def scipy_newton(f):
+    scipy_res = minimize(
+        f,
+        np.array([5, 2], dtype=float),
+        method='Newton-CG',
+        jac=lambda x: grad_f(f, x[0], x[1]),
+        hessp=hessp_factory(f),
+        options={"maxiter": 2000, "disp": False, "xtol": np.finfo(float).eps},
+    )
+    if not scipy_res.success:
+        print("scipy KvasiNewton method failed to converge")
+    else:
+        print(
+            f"scipy.optimize.minimize(..., method=\"Newton-CG\") result: x={scipy_res.x[0]}, y={scipy_res.x[1]}")
+
+
+def scipy_bfgs(f):
+    scipy_res = minimize(
+        f,
+        np.array([5, 2], dtype=float),
+        method='BFGS',
+        options={'maxiter': 2000, 'disp': True},
+    )
+    if not scipy_res.success:
+        print("scipy Newton method failed to converge")
+    else:
+        print(
+            f"scipy.optimize.minimize(..., method=\"BFGS\") result: x={scipy_res.x[0]}, y={scipy_res.x[1]}")
 
 
 def print_result():
@@ -20,16 +65,28 @@ def print_result():
         for function in functions:
             print(function[1])
             print("=" * 50)
+            if method == "Nelder-Mid":
+                scipy_res = minimize(function[0], np.array([5, 2], dtype=float), method='Nelder-Mead')
+                if not scipy_res.success:
+                    print("scipy Nelder-Mid method failed to converge")
+                else:
+                    print(f"scipy.optimize.minimize(..., method=\"Nelder-Mid\") result: x={scipy_res.x[0]}, y={scipy_res.x[1]}")
+                print("=" * 50)
+                continue
             if method == "decreasing_lr":
-                coords = gradient_descent(function[0], 100, 2, method=method, h=0.1, iterations=50000)
+                coords = gradient_descent(function[0], 5, 2, method=method, h=0.1, newton_h=1.5, iterations=50000)
             else:
-                coords = gradient_descent(function[0], 100, 2, method=method, stop=1e-6)
+                coords = gradient_descent(function[0], 5, 2, method=method, newton_h=1.5)
             if method == "Armijo" or method == "Goldstein":
                 print(f"Scipy.optimize.line_search result: x={coords[2]}, y={coords[3]}")
             if method == "golden_section":
                 print(f"Scipy.optimize.minimize_scalar(..., method=\"golden\") result: x={coords[2]}, y={coords[3]}")
             if method == "parabolic":
                 print(f"Scipy.optimize.minimize_scalar(..., method=\"brent\") result: x={coords[2]}, y={coords[3]}")
+            if method == "newton" or method == "newton_armijo":
+                scipy_newton(function[0])
+            if method == "bfgs":
+                scipy_bfgs(function[0])
             print(f"x={coords[0]}, y={coords[1]}")
             print(f"func_count={coords[4][0]}, grad_count={coords[4][1]}, iter_count={coords[4][2]}")
             print("=" * 50)
